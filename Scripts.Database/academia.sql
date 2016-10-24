69d347ce33471e3285263e45e87f40c37897d5d7
FacundoSegarra <facundosegarra@outlook.com>
2016-10-24 15:55:57 -0300

    Mejorando la creación de la base de datos.

--- Scripts.Database/academia.sql
+++ Scripts.Database/academia.sql
@@ -1,59 +1,30 @@
-CREATE DATABASE  IF NOT EXISTS `academia` /*!40100 DEFAULT CHARACTER SET latin1 */;
+DROP DATABASE IF EXISTS `academia`;
+
+CREATE DATABASE  IF NOT EXISTS `academia`;
+
 USE `academia`;
--- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: academia
--- ------------------------------------------------------
--- Server version	5.7.14
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+DROP TABLE IF EXISTS `especialidades`;
 
---
--- Table structure for table `alumnos_inscripciones`
---
+CREATE TABLE `especialidades` (
+  `id_especialidad` int(11) NOT NULL AUTO_INCREMENT,
+  `desc_especialidad` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_especialidad`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `alumnos_inscripciones`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `alumnos_inscripciones` (
-  `id_inscripcion` int(11) NOT NULL AUTO_INCREMENT,
-  `id_alumno` int(11) NOT NULL,
-  `id_curso` int(11) NOT NULL,
-  `condicion` varchar(50) DEFAULT NULL,
-  `nota` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_inscripcion`),
-  KEY `id_curso` (`id_curso`),
-  KEY `id_alumno` (`id_alumno`),
-  CONSTRAINT `alumnos_inscripciones_ibfk_1` FOREIGN KEY (`id_curso`) REFERENCES `cursos` (`id_curso`) ON UPDATE CASCADE,
-  CONSTRAINT `alumnos_inscripciones_ibfk_2` FOREIGN KEY (`id_alumno`) REFERENCES `personas` (`id_persona`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+DROP TABLE IF EXISTS `planes`;
 
---
--- Dumping data for table `alumnos_inscripciones`
---
-
-LOCK TABLES `alumnos_inscripciones` WRITE;
-/*!40000 ALTER TABLE `alumnos_inscripciones` DISABLE KEYS */;
-/*!40000 ALTER TABLE `alumnos_inscripciones` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `comisiones`
---
+CREATE TABLE `planes` (
+  `id_plan` int(11) NOT NULL AUTO_INCREMENT,
+  `desc_plan` varchar(50) NOT NULL,
+  `id_especialidad` int(11) NOT NULL,
+  PRIMARY KEY (`id_plan`),
+  KEY `id_especialidad` (`id_especialidad`),
+  CONSTRAINT `planes_ibfk_1` FOREIGN KEY (`id_especialidad`) REFERENCES `especialidades` (`id_especialidad`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `comisiones`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `comisiones` (
   `id_comision` int(11) NOT NULL AUTO_INCREMENT,
   `desc_comision` varchar(50) NOT NULL,
@@ -63,24 +34,22 @@ CREATE TABLE `comisiones` (
   KEY `id_plan` (`id_plan`),
   CONSTRAINT `comisiones_ibfk_1` FOREIGN KEY (`id_plan`) REFERENCES `planes` (`id_plan`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `comisiones`
---
+DROP TABLE IF EXISTS `materias`;
 
-LOCK TABLES `comisiones` WRITE;
-/*!40000 ALTER TABLE `comisiones` DISABLE KEYS */;
-/*!40000 ALTER TABLE `comisiones` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `cursos`
---
+CREATE TABLE `materias` (
+  `id_materia` int(11) NOT NULL AUTO_INCREMENT,
+  `desc_materia` varchar(50) NOT NULL,
+  `horas_semanales` int(11) DEFAULT NULL,
+  `hs_totales` int(11) DEFAULT NULL,
+  `id_plan` int(11) NOT NULL,
+  PRIMARY KEY (`id_materia`),
+  KEY `id_plan` (`id_plan`),
+  CONSTRAINT `materias_ibfk_1` FOREIGN KEY (`id_plan`) REFERENCES `planes` (`id_plan`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `cursos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `cursos` (
   `id_curso` int(11) NOT NULL AUTO_INCREMENT,
   `id_materia` int(11) NOT NULL,
@@ -93,159 +62,9 @@ CREATE TABLE `cursos` (
   CONSTRAINT `cursos_ibfk_1` FOREIGN KEY (`id_comision`) REFERENCES `comisiones` (`id_comision`) ON UPDATE CASCADE,
   CONSTRAINT `cursos_ibfk_2` FOREIGN KEY (`id_materia`) REFERENCES `materias` (`id_materia`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `cursos`
---
-
-LOCK TABLES `cursos` WRITE;
-/*!40000 ALTER TABLE `cursos` DISABLE KEYS */;
-/*!40000 ALTER TABLE `cursos` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `docentes_cursos`
---
-
-DROP TABLE IF EXISTS `docentes_cursos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `docentes_cursos` (
-  `id_dictado` int(11) NOT NULL AUTO_INCREMENT,
-  `id_curso` int(11) NOT NULL,
-  `id_docente` int(11) NOT NULL,
-  `cargo` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_dictado`),
-  KEY `id_docente` (`id_docente`),
-  CONSTRAINT `docentes_cursos_ibfk_1` FOREIGN KEY (`id_docente`) REFERENCES `personas` (`id_persona`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `docentes_cursos`
---
-
-LOCK TABLES `docentes_cursos` WRITE;
-/*!40000 ALTER TABLE `docentes_cursos` DISABLE KEYS */;
-/*!40000 ALTER TABLE `docentes_cursos` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `especialidades`
---
-
-DROP TABLE IF EXISTS `especialidades`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `especialidades` (
-  `id_especialidad` int(11) NOT NULL AUTO_INCREMENT,
-  `desc_especialidad` varchar(50) NOT NULL,
-  PRIMARY KEY (`id_especialidad`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `especialidades`
---
-
-LOCK TABLES `especialidades` WRITE;
-/*!40000 ALTER TABLE `especialidades` DISABLE KEYS */;
-INSERT INTO `especialidades` VALUES (2,'ISI'),(3,'Mecánica'),(4,'Civil');
-/*!40000 ALTER TABLE `especialidades` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `materias`
---
-
-DROP TABLE IF EXISTS `materias`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `materias` (
-  `id_materia` int(11) NOT NULL AUTO_INCREMENT,
-  `desc_materia` varchar(50) NOT NULL,
-  `horas_semanales` int(11) DEFAULT NULL,
-  `hs_totales` int(11) DEFAULT NULL,
-  `id_plan` int(11) NOT NULL,
-  PRIMARY KEY (`id_materia`),
-  KEY `id_plan` (`id_plan`),
-  CONSTRAINT `materias_ibfk_1` FOREIGN KEY (`id_plan`) REFERENCES `planes` (`id_plan`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `materias`
---
-
-LOCK TABLES `materias` WRITE;
-/*!40000 ALTER TABLE `materias` DISABLE KEYS */;
-/*!40000 ALTER TABLE `materias` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `modulos`
---
-
-DROP TABLE IF EXISTS `modulos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `modulos` (
-  `id_modulo` int(11) NOT NULL AUTO_INCREMENT,
-  `desc_modulo` varchar(50) NOT NULL,
-  `ejecuta` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id_modulo`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `modulos`
---
-
-LOCK TABLES `modulos` WRITE;
-/*!40000 ALTER TABLE `modulos` DISABLE KEYS */;
-/*!40000 ALTER TABLE `modulos` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `modulos_usuarios`
---
-
-DROP TABLE IF EXISTS `modulos_usuarios`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `modulos_usuarios` (
-  `id_modulo_usuario` int(11) NOT NULL AUTO_INCREMENT,
-  `id_modulo` int(11) NOT NULL,
-  `id_usuario` int(11) NOT NULL,
-  `alta` bit(1) DEFAULT NULL,
-  `baja` bit(1) DEFAULT NULL,
-  `modificacion` bit(1) DEFAULT NULL,
-  `consulta` bit(1) DEFAULT NULL,
-  PRIMARY KEY (`id_modulo_usuario`),
-  KEY `id_usuario` (`id_usuario`),
-  KEY `id_modulo` (`id_modulo`),
-  CONSTRAINT `modulos_usuarios_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `modulos_usuarios_ibfk_2` FOREIGN KEY (`id_modulo`) REFERENCES `modulos` (`id_modulo`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `modulos_usuarios`
---
-
-LOCK TABLES `modulos_usuarios` WRITE;
-/*!40000 ALTER TABLE `modulos_usuarios` DISABLE KEYS */;
-/*!40000 ALTER TABLE `modulos_usuarios` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `personas`
---
 
 DROP TABLE IF EXISTS `personas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `personas` (
   `id_persona` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL,
@@ -261,52 +80,45 @@ CREATE TABLE `personas` (
   KEY `id_plan` (`id_plan`),
   CONSTRAINT `personas_ibfk_1` FOREIGN KEY (`id_plan`) REFERENCES `planes` (`id_plan`) ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `personas`
---
+DROP TABLE IF EXISTS `alumnos_inscripciones`;
 
-LOCK TABLES `personas` WRITE;
-/*!40000 ALTER TABLE `personas` DISABLE KEYS */;
-INSERT INTO `personas` VALUES (2,'Facundo','Segarra','Maipú 686','facu@gmail.com','(03464) 15448309','1987-03-08 00:00:00',32412,'alumno',1),(3,'Ernesto','Bugdahl','San Lorenzo 930','vitty@gmail.com','(0341) 153214221','1999-07-21 00:00:00',42332,'alumno',1);
-/*!40000 ALTER TABLE `personas` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE `alumnos_inscripciones` (
+  `id_inscripcion` int(11) NOT NULL AUTO_INCREMENT,
+  `id_alumno` int(11) NOT NULL,
+  `id_curso` int(11) NOT NULL,
+  `condicion` varchar(50) DEFAULT NULL,
+  `nota` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_inscripcion`),
+  KEY `id_curso` (`id_curso`),
+  KEY `id_alumno` (`id_alumno`),
+  CONSTRAINT `alumnos_inscripciones_ibfk_1` FOREIGN KEY (`id_curso`) REFERENCES `cursos` (`id_curso`) ON UPDATE CASCADE,
+  CONSTRAINT `alumnos_inscripciones_ibfk_2` FOREIGN KEY (`id_alumno`) REFERENCES `personas` (`id_persona`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `planes`
---
+DROP TABLE IF EXISTS `docentes_cursos`;
 
-DROP TABLE IF EXISTS `planes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `planes` (
-  `id_plan` int(11) NOT NULL AUTO_INCREMENT,
-  `desc_plan` varchar(50) NOT NULL,
-  `id_especialidad` int(11) NOT NULL,
-  PRIMARY KEY (`id_plan`),
-  KEY `id_especialidad` (`id_especialidad`),
-  CONSTRAINT `planes_ibfk_1` FOREIGN KEY (`id_especialidad`) REFERENCES `especialidades` (`id_especialidad`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE `docentes_cursos` (
+  `id_dictado` int(11) NOT NULL AUTO_INCREMENT,
+  `id_curso` int(11) NOT NULL,
+  `id_docente` int(11) NOT NULL,
+  `cargo` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_dictado`),
+  KEY `id_docente` (`id_docente`),
+  CONSTRAINT `docentes_cursos_ibfk_1` FOREIGN KEY (`id_docente`) REFERENCES `personas` (`id_persona`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `planes`
---
+DROP TABLE IF EXISTS `modulos`;
 
-LOCK TABLES `planes` WRITE;
-/*!40000 ALTER TABLE `planes` DISABLE KEYS */;
-INSERT INTO `planes` VALUES (1,'ISI- 2005',2),(2,'Mecánica - 2005',3),(3,'Civil - 2005',4),(4,'ISI - 2008',2);
-/*!40000 ALTER TABLE `planes` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `usuarios`
---
+CREATE TABLE `modulos` (
+  `id_modulo` int(11) NOT NULL AUTO_INCREMENT,
+  `desc_modulo` varchar(50) NOT NULL,
+  `ejecuta` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_modulo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `usuarios`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `usuarios` (
   `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
   `nombre_usuario` varchar(50) NOT NULL,
@@ -321,25 +133,29 @@ CREATE TABLE `usuarios` (
   KEY `id_persona` (`id_persona`),
   CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`id_persona`) REFERENCES `personas` (`id_persona`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `usuarios`
---
+DROP TABLE IF EXISTS `modulos_usuarios`;
 
-LOCK TABLES `usuarios` WRITE;
-/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+CREATE TABLE `modulos_usuarios` (
+  `id_modulo_usuario` int(11) NOT NULL AUTO_INCREMENT,
+  `id_modulo` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `alta` bit(1) DEFAULT NULL,
+  `baja` bit(1) DEFAULT NULL,
+  `modificacion` bit(1) DEFAULT NULL,
+  `consulta` bit(1) DEFAULT NULL,
+  PRIMARY KEY (`id_modulo_usuario`),
+  KEY `id_usuario` (`id_usuario`),
+  KEY `id_modulo` (`id_modulo`),
+  CONSTRAINT `modulos_usuarios_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `modulos_usuarios_ibfk_2` FOREIGN KEY (`id_modulo`) REFERENCES `modulos` (`id_modulo`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+INSERT INTO `especialidades` VALUES (2,'ISI'),(3,'Mecánica'),(4,'Civil');
+
+INSERT INTO `planes` VALUES (1,'ISI- 2005',2),(2,'Mecánica - 2005',3),(3,'Civil - 2005',4),(4,'ISI - 2008',2);
+
+INSERT INTO `personas` VALUES (2,'Facundo','Segarra','Maipú 686','facu@gmail.com','(03464) 15448309','1987-03-08 00:00:00',32412,'alumno',1),(3,'Ernesto','Bugdahl','San Lorenzo 930','vitty@gmail.com','(0341) 153214221','1999-07-21 00:00:00',42332,'alumno',1);
+
 INSERT INTO `usuarios` VALUES (1,'Vitty','3caracoles',1,'Vitty','Bugdahl','ebugdahl@gmail.com',0,NULL),(2,'Facu','gabibatistuta',0,'Facundo','Gardella','facugardella@hotmail.com',0,NULL),(4,'fer','onomatopeya',1,'Fernando','Duarte','fer@yahoo.com',0,NULL);
-/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-10-24 13:23:18
