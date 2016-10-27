36438d9ec69489768fddb967b691f47112e59e6d
FacundoSegarra <facundosegarra@outlook.com>
2016-10-27 05:46:29 -0300

    Implementado try catch personalizados

--- Business.Logic/EspecialidadLogic.cs
+++ Business.Logic/EspecialidadLogic.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Business.Entities;
 using Data.Database;
+using Util.CustomException;
 
 namespace Business.Logic
 {
@@ -12,27 +13,51 @@ namespace Business.Logic
     {
         private EspecialidadAdapter especialidadData;
 
-        public List<Especialidad> getAll()
+        public EspecialidadLogic()
         {
-            List<Especialidad> listaEspecialidades = new List<Especialidad>();
-            especialidadData = new EspecialidadAdapter();
-            listaEspecialidades = especialidadData.GetAll();
-            return listaEspecialidades;
+            this.especialidadData = new EspecialidadAdapter();
+        }
+
+        public List<Especialidad> GetAll()
+        {
+            try
+            { 
+                List<Especialidad> listaEspecialidades = new List<Especialidad>();
+                listaEspecialidades = this.especialidadData.GetAll();
+                return listaEspecialidades;
+            }
+            catch (NotFoundException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw new CustomException(ex);
+            }
         }
 
         public void Save(Especialidad e)
         {
-            especialidadData = new EspecialidadAdapter();
-            especialidadData.Save(e);
+            this.especialidadData.Save(e);
 
         }
 
         public Especialidad GetOne(int id)
         {
-            Especialidad e = new Especialidad();
-            especialidadData = new EspecialidadAdapter();
-            e = especialidadData.GetOne(id);
-            return e;
+            try
+            {
+                Especialidad e = new Especialidad();
+                e = this.especialidadData.GetOne(id);
+                return e;
+            }
+            catch (NotFoundException ex)
+            {
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                throw new CustomException(ex);
+            }
         }
     }
 }
