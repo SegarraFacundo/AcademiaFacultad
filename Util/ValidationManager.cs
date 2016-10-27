89d5b444995ad8c48d940b454c1773b252fc6f8d
FacundoSegarra <facundosegarra@outlook.com>
2016-10-27 05:50:14 -0300

    Clase para personalizar las validaciones e darle reusabilidad.

---
+++ Util/ValidationManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Util
+{
+    public class ValidationManager
+    {
+        public static bool Number(string value)
+        {
+            Int32.Parse(value);
+            return true;
+        }
+
+        public static bool Min()
+        {
+            return true;
+        }
+
+        public static bool Max()
+        {
+            return true;
+        }
+
+        public static bool Required(Object value)
+        {
+            if (value.ToString() == "")
+                return false;
+            return true;
+        }
+
+        public static bool Equal(Object value1, Object value2)
+        {
+            return (value1.Equals(value2));
+        }
+    }
+}
