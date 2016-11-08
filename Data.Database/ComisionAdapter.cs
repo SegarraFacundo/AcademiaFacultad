25f35cc03222d56fee31dbf85fce843227184bf2
alecast <a.castano.landin@gmail.com>
2016-11-07 22:41:06 -0300

    ComisionesAdapter, Inicio WebForm Comisiones

--- Data.Database/ComisionAdapter.cs
+++ Data.Database/ComisionAdapter.cs
@@ -4,20 +4,155 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Business.Entities;
+using MySql.Data.MySqlClient;
+using Util.CustomException;
+using Util;
 
 namespace Data.Database
 {
     class ComisionAdapter: Adapter
     {
+        
         public List<Comision> GetAll()
         {
-            return new List<Comision>();
+            List<Comision> listaComisiones = new List<Comision>();
+            try
+            {
+                this.OpenConnection();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM comisiones", MySqlConn);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Comision c = new Comision();
+                    c.Id = (int)reader["id_comision"];
+                    c.IdPlan = (int)reader["id_plan"];
+                    c.Descripcion = (string)reader["desc_comision"];
+                    c.AnioEspecialidad = (int)reader["anio_especialidad"];
+                    listaComisiones.Add(c);
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                throw new NotFoundException("comision", ex);
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+            return listaComisiones;
         }
-        public Comision GetOne(int Id)
+        public Comision GetOne(int id)
         {
-            return new Comision();
+            Comision c;
+            try
+            {
+                this.OpenConnection();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM comisiones WHERE id_comision = @id", MySqlConn);
+                cmd.Parameters.AddWithValue("@id", id);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    c = new Comision();
+                    c.Id = id;
+                    c.IdPlan = (int)reader["id_plan"];
+                    c.Descripcion = (string)reader["desc_comision"];
+                    c.AnioEspecialidad = (int)reader["anio_especialidad"];
+                    reader.Close();
+                    return c;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new NotFoundException("comision", ex);
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+            return null;
         }
 
+        public void Insert(Comision c)
+        {
+            try
+            {
+                this.OpenConnection();
+                MySqlCommand cmd = new MySqlCommand("INSERT INTO comisiones (desc_comision) VALUES (@comision); SELECT @@IDENTITY;", MySqlConn);
+                cmd.Parameters.AddWithValue("@comision", c.Descripcion);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new InsertException("comision", ex);
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
 
+        public void Update(Comision c)
+        {
+            try
+            {
+                this.OpenConnection();
+                MySqlCommand cmd = new MySqlCommand("UPDATE comisiones SET desc_comision = @comision WHERE id_comision = @idcomision", MySqlConn);
+                cmd.Parameters.AddWithValue("@descripcion", c.Descripcion);
+                cmd.Parameters.AddWithValue("@idcomision", c.Id);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new UpdateException("comision", ex);
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            try
+            {
+                this.OpenConnection();
+
+                MySqlCommand cmd = new MySqlCommand("DELETE FROM comisiones WHERE id_comision = @id", MySqlConn);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new DeleteException("comision", ex);
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+        public void Save(Comision c)
+        {
+
+            if (c.State == TiposDatos.States.New)
+            {
+                this.Insert(c);
+            }
+            else if (c.State == TiposDatos.States.Modified)
+            {
+                this.Update(c);
+            }
+            else if (c.State == TiposDatos.States.Deleted)
+            {
+                this.Delete(c.Id);
+            }
+            else
+            {
+                c.State = TiposDatos.States.Unmodified;
+            }
+
+        }
     }
 }
+
+
